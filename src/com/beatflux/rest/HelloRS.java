b79e53d164454a982780887fcd1c84a614d76597
U-INFOR\eyousefi <eyousefi@SEKINEYOUSEFI02.infor.com>
2016-04-06 17:57:26 +0200

    Project setup

---
+++ src/com/beatflux/rest/HelloRS.java
@@ -0,0 +1,19 @@
+package com.beatflux.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloRS {
+   @GET
+   public Response greet(@QueryParam("name") String name) {
+      if (name == null)
+         name = "Stranger";
+      return Response.ok("Hello " + name + "!").build();
+   }
+}
