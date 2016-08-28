6f2f59ddf0ba4eafed7502486ffde85810e97600
Nasrin Farhat <nasrin.farhat@smartoptics.com>
2016-08-28 20:49:02 +0200

    Added some test cases for junit on SpotDal.

--- src/test/java/com/beatflux/db/SpotDALTests.java
+++ src/test/java/com/beatflux/db/SpotDALTests.java
@@ -2,7 +2,10 @@ package com.beatflux.db;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,20 +14,71 @@ import com.beatflux.db.dal.SpotDAL;
 import com.beatflux.db.to.SpotTO;
 
 public class SpotDALTests {
-	SpotDAL dal = new SpotDAL();
-	SpotTO spot = new SpotTO();
-	@Test 
-	public void testListUsers() {
-		List<SpotTO> spotList = dal.listSpots();
-		assertFalse(spotList.isEmpty());
-	}
-	@Test
-	public void testSearchSpot() {
-		assertNotNull("object should exist", dal.searchRecord(1));
-	}
-	@Test
-	public void testDeleteSpot() {
-		dal.deleteSpot(1);
-		assertNotNull("object should not exist", dal.searchRecord(1));
-	}
+   SpotDAL dal = new SpotDAL();
+   SpotTO spot = new SpotTO();
+
+   @Test 
+   public void testAddSpot(){
+      Date date = new Date(System.currentTimeMillis()); 
+      long time = date.getTime();
+      
+      spot.setUserName("test");
+      spot.setEmail("nasrin.nahidErfan@gmail.com");
+      spot.setEquipment("Machine");
+      spot.setName("test");
+      spot.setPassword("1234");
+      spot.setPhoneNumber("72395868929");
+      spot.setSignupTimstamp(new Timestamp(time));
+      spot.setLastOnline(new Timestamp(time));
+      spot.setCountryCode("se");
+      
+      dal.addSpot(spot);
+      String email = spot.getEmail();
+      assertTrue("object should exist in DataBase", dal.emailExist(email));
+   }
+   
+   @Test 
+   public void testUpdateSpot(){
+      Date date = new Date(System.currentTimeMillis()); 
+      long time = date.getTime();
+      
+      spot.setUserName("testUpdate");
+      spot.setEmail("nasrin.nahid@gmail.com");
+      spot.setEquipment("Machine");
+      spot.setName("testUpdate");
+      spot.setPassword("1234");
+      spot.setPhoneNumber("723958689");
+      spot.setSignupTimstamp(new Timestamp(time));
+      spot.setLastOnline(new Timestamp(time));
+      spot.setCountryCode("se");
+      
+      if (dal.emailExist("nasrin.nahid@gmail.com")){
+      String email = spot.getEmail();
+      assertTrue("object should exist in DataBase", dal.emailExist(email));
+      } else {
+         System.out.print("Eamil does not exist!");
+      }
+   }
+
+   @Test 
+   public void testListUsers() {
+      List<SpotTO> spotList = dal.listSpot();
+      assertFalse(spotList.isEmpty());
+   }
+   @Test
+   public void testSearchSpot() {
+      assertNotNull("object should exist", dal.checkRecord(1));
+   }
+   @Test
+   public void testDeleteSpot() {
+      String email = "s1@gmail.com";
+      dal.deleteSpot(email);
+      assertFalse("object should not exist", dal.emailExist(email));
+   }
+   
+   @Test
+   public void TestGetSpot(){
+      String email = "nasrin.nahid@gmail.com";
+     assertTrue("Fail!object is not exist",dal.emailExist(dal.getSpot(email).getEmail()));   
+   }
 }
